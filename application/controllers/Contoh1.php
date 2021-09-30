11d9e7f1f67c8196eb1a42ce2a8140ba0ae493f5
nurikhairunisayumasyifa <nurikhairunisay@gmail.com>
2021-09-30 11:43:48 +0700

    menambahkan application/,system/,user_guide/

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Biodata Diri</h1>";
+        echo"Nama : Nuri Khairunisa Yuma Syifa <br>
+        TTL : Bekasi, 27 April 2002 <br>
+        Alamat : Perumahan Rajeg Gardenia <br>
+        Hobi : Nonton K-drama dan editing foto <br>";
+    }
+}
